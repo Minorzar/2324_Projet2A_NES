5cbc742f5cbb95f8b54dbd5ab3d84a0da22895ed
Minorzar <gauthier_biehler@yahoo.com>
2023-11-14 20:28:59 +0100

    Update VGA: VGA_Sync working

---
+++ software/Proc_Quartus/VGA_Sync.vhd
@@ -0,0 +1,37 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity VGA_Sync is
+    Port(
+        clk25   : in std_logic;
+        reset   : in std_logic;
+        hs      : out std_logic;
+        vs      : out std_logic;
+        inDisp  : out std_logic;
+        x       : out std_logic_vector(9 downto 0);
+        y       : out std_logic_vector(9 downto 0)
+    );
+end VGA_Sync;
+
+architecture behavioral of VGA_Sync is
+    signal EN       : std_logic;
+    signal dispH    : std_logic;
+    signal dispV    : std_logic;
+    signal VS_sig   : std_logic;
+    signal HS_sig   : std_logic;
+    signal x_val    : std_logic_vector(9 downto 0);
+    signal y_val    : std_logic_vector(9 downto 0);
+
+begin
+    u1 : entity work.H_sync
+        port map (clk25 => clk25, reset => reset, cnt_y_enable => EN, H_S => HS_sig, inDispH => dispH, x_value => x_val);
+    u2 : entity work.V_sync
+        port map (clk25 => clk25, reset => reset, cnt_y_enable => EN, V_S => VS_sig, inDispV => dispV, y_value => y_val);
+
+    hs <= HS_sig;
+    vs <= VS_sig;
+    inDisp <= (dispH and dispV);
+    x <= x_val;
+    y <= y_val;
+
+end behavioral;
---
+++ software/Proc_Quartus/V_sync.vhd
@@ -0,0 +1,62 @@
+Library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity V_sync is
+	Port(
+		clk25: in std_logic;
+		reset : in std_logic;
+		V_S : out std_logic;
+		inDispV : out std_logic;
+		cnt_y_enable : in std_logic;
+		y_value : out std_logic_vector(9 downto 0));
+end V_sync;
+
+ 
+--		Assuming we use VGA for 60Hz, 640x480 it comes:
+--			
+--			VERTICAL TIMING
+--			
+--				- a : 2 lines		=> Sync (HSync low)									=> 1585 clock cycles (or 63.4us)
+--				- b : 33 lines		=> back porch (before display)					=> 26152.5 clock cycles (or 1046,1us)
+--				- c : 480 lines	=> display time										=> 380400 clock cycles (or 15216us)
+--				- d : 10 lines		=> front porch (after display, before sync)	=> 7925 clock cycles (or 317us)
+--				
+--			=> 416062.5 clock cycles (or 16.6424ms) per frame
+--			
+--			Also meaning:
+--				
+--				- a : 2 lines => 1 lines
+--				- b : 33 lines => 34 lines
+--				- c : 480 lines => 514 lines
+--				- d : 10 lines => 524 lines
+--			
+--			Hence: a counter from 0 to 525 that restart at 525 and set inDispV between the 34th and 513rd line.
+--			Mostly, it gives us:
+--			counter between 0 and 1 => VS = 0, inDispV = 0,
+--			counter between 2 and 34 => VS = 1, inDispV = 0,
+--			counter between 35 and 514 => VS = 1, inDispV = 1,
+--			counter between 515 and 524 => VS = 0, inDispV = 0.
+
+
+architecture Behavioral of V_sync is
+	signal counter : natural range 0 to 525;
+	
+begin
+	
+	process(clk25)
+	begin
+		if (reset = '1') then
+			counter <= 0 ;
+		elsif (rising_edge(clk25) and (cnt_y_enable = '1')) then
+			counter <= counter + 1 ;
+			if (counter = 525) then
+				counter <= 0 ;
+			end if ;
+		end if ;
+	end process ;
+
+	V_S <= '1' when (counter > 1 and counter < 35) else '0' ;
+	inDispV <= '1' when (counter > 34 and counter < 515) else '0' ;
+	
+
+end Behavioral;
