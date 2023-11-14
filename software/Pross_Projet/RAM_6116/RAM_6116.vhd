bacc7da495d30417273244f66f38eb69ea1e01cf
Robaur <auriac.robin.pro@gmail.com>
2023-11-14 15:59:39 +0100

    FINISHED

--- software/Pross_Projet/RAM_6116/RAM_6116.vhd
+++ software/Pross_Projet/RAM_6116/RAM_6116.vhd
@@ -5,13 +5,15 @@ use ieee.numeric_std.all;
 entity RAM_6116 is 
     port(
     --clock
+    clk : in std_logic;
 
-    CS: in std_logic;
+    -- chip select
+    CS_n: in std_logic;
 
     -- enable 
-    WE: in std_logic;
+    WE_n: in std_logic;
     -- desable output
-    OE: in std_logic;
+    OE_n: in std_logic;
      
     -- all pin to select a row (pin coding an adress)
 
@@ -25,22 +27,36 @@ end RAM_6116;
 
 architecture RAM_6116_rft of RAM_6116 is 
     subtype word_8 is unsigned(7 downto 0);
-    type MEMORY is array(2**10 downto 0) of word_8;
+    type MEMORY is array(0 to (2**11)-1) of word_8;
     signal RAM: MEMORY;
+
+-- Créer port d'entrée et de sortie
+signal s_in_port : unsigned(7 downto 0);
+signal s_out_port : unsigned(7 downto 0);
 
     begin 
 
-    process(CS)
-    begin
-            if (CS='0') then
-                if(WE='1') then 
-                    if(OE='0') then 
-                        io_data <=RAM(to_integer(i_Adress));
-                    end if;
+    --OLD ONES
 
-                else
-                    RAM(to_integer(i_Adress))<=io_data;
-                end if;
-            end if;
-    end process;
+
+    -- TODO réécrire une ram standard (avec deux ports: signaux internes)
+   process(clk)
+   begin
+        if(rising_edge(clk)) then 
+           if (CS_n='0') then
+               if(WE_n='1') then  
+                    s_out_port <=RAM(to_integer(i_Adress));
+               else
+                   RAM(to_integer(i_Adress))<=s_in_port;
+               end if;
+           end if;
+        end if;
+   end process;
+
+   s_in_port <= io_data; 
+   -- pour faire en sorte que la sortie devienne l'entré
+
+   io_data <= s_out_port when (OE_n = '0') else "ZZZZZZZZ";
+   --pour faire en sorte que ce qui sort devient ce qui est bien présen
+
 end RAM_6116_rft; 
--- software/Pross_Projet/RAM_6116/RAM_6116tb.vhd
+++ software/Pross_Projet/RAM_6116/RAM_6116tb.vhd
@@ -9,6 +9,7 @@ architecture R_6116 of RAM_6116tb is
     signal CS: std_logic;
     signal WE: std_logic;
     signal OE: std_logic;
+    signal clk: std_logic;
     signal iAdress: unsigned(10 downto 0);
     signal iodata: unsigned(7 downto 0);
 
@@ -19,46 +20,60 @@ architecture R_6116 of RAM_6116tb is
 begin
     RAM : entity work.RAM_6116
     port map(
-        CS=>CS,
-        WE=>WE,
-        OE=>OE,
+        CS_n=>CS,
+        WE_n=>WE,
+        OE_n=>OE,
         i_Adress=>iAdress,
-        io_data=>iodata
+        io_data=>iodata,
+        clk=>clk
     );
 
     benche : process
+    -- quand on s'attend pas recesoir un donnée on doit ce mettre en Z
         begin
+                
                 WE<='1';
-                OE<='0';
+                OE<='1';
+                CS<='1';
+                iodata<="ZZZZZZZZ";
+            --    iodata <= (others => 'Z');
             wait for 3* CLK_PERIOD;
-                iodata<=to_unsigned(00010011,8);
-                iAdress<=to_unsigned(0000000001,10);
+                iAdress<="00000000001";
             wait for 3* CLK_PERIOD;
-                iodata<=to_unsigned(00010011,8);
-                iAdress<=to_unsigned(0000000010,10);
-            wait for 3* CLK_PERIOD;
-                WE<='1';
-            wait for 3* CLK_PERIOD;
-                iodata<=to_unsigned(00010011,8);
-                iAdress<=to_unsigned(0000000011,10);
-            wait for 3* CLK_PERIOD;
-                WE<='0';
-                iAdress<=to_unsigned(0000000010,10);
-            wait for 3* CLK_PERIOD;
-                 OE<='1';
-             wait for 3* CLK_PERIOD;
-                iAdress<=to_unsigned(0000000001,10);
+                CS<='0';
+                iodata<="ZZZZZZZZ" ;
+                iAdress<="00000000010";
 
+
+            wait for 3* CLK_PERIOD;
+                --write begin
+                WE<='0';
+            wait for 3* CLK_PERIOD;
+                iodata<="00010011";
+                iAdress<="00000000001";
+            wait for 3* CLK_PERIOD;
+                --read 
+                WE<='1';
+                iAdress<="00000000010";
+                iodata <= (others => 'Z');
+            wait for 3* CLK_PERIOD;
+                 OE<='0';
+            wait for 3* CLK_PERIOD;
+                iAdress<="00000000001";
+            wait for 3* CLK_PERIOD;
+                finished <= true;
+            wait;
     end process benche;
 
     p_clk: process
     begin 
          while (finished =false) loop
-            CS<='0';
+            clk<='0';
             wait for CLK_PERIOD/2;
-            CS<='1';
+            clk<='1';
             wait for CLK_PERIOD/2;
         end loop ; -- identifier
+        wait;
     end process p_clk;
 
 end R_6116 ; -- R_6116
