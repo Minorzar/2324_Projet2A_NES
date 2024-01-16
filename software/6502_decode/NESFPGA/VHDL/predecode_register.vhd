facaed94387887537373ef9af2796626039a4110
NightFore <Et3rnNighTaLL@Hotmail.com>
2024-01-16 17:32:12 +0100

    #12 Updated predecode_register, predecode_logic, instruction_register, timing_generation logic

--- software/6502_decode/NESFPGA/VHDL/predecode_register.vhd
+++ software/6502_decode/NESFPGA/VHDL/predecode_register.vhd
@@ -1,28 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
 entity predecode_register is
 	Port (
-		i_databus : in STD_LOGIC_VECTOR(7 downto 0);					-- Input data bus
-		i_clk : in STD_LOGIC;												-- Clock signal
-		i_clear_buffer : in STD_LOGIC;									-- Clear signal for the buffer
-		o_predecode_buffer : out STD_LOGIC_VECTOR(7 downto 0)		-- Output predecode buffer
+		i_clk : in STD_LOGIC;											-- Clock signal
+		i_databus : in STD_LOGIC_VECTOR(7 downto 0);				-- Input data bus
+		o_instruction : out STD_LOGIC_VECTOR(7 downto 0)		-- Output instruction
 	);
 end predecode_register;
 
 architecture Behavioral of predecode_register is
-	signal s_predecode_buffer : unsigned(7 downto 0) := (others => '0');
+	signal s_predecode_buffer : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
 begin
 	-- Main process
-	process(i_clk, i_clear_buffer)
+	process(i_clk)
 	begin
-		if i_clear_buffer = '1' then
-			-- Reset the buffer in case of an active clear signal
-			s_predecode_buffer <= (others => '0');
-		elsif rising_edge(i_clk) then
+		if rising_edge(i_clk) then
 			-- Load the buffer with data from the data bus
-			s_predecode_buffer <= unsigned(i_databus);
+			s_predecode_buffer <= i_databus;
 		end if;
 	end process;
 
 	-- Output the buffer
-	o_predecode_buffer <= std_logic_vector(s_predecode_buffer);
+	o_instruction <= s_predecode_buffer;
 
 end Behavioral;
