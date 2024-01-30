ac796315d0ff3ab7686c69d3855ed987af61e064
NightFore <Et3rnNighTaLL@Hotmail.com>
2024-01-30 16:05:21 +0100

    Merging

--- software/VHDL/Predecode/VHDL/decode_tb.vhd
+++ software/VHDL/Predecode/VHDL/decode_tb.vhd
@@ -87,7 +87,7 @@ begin
 	uut_predecode_register: predecode_register
 		port map (
 			i_clk => t_clk_1,
-			i_reset => '0',  -- No reset in this testbench
+			i_reset => '0',	-- No reset in this testbench
 			i_db_instruction => t_db_instruction,
 			o_pr_instruction => t_pr_instruction
 		);
@@ -96,7 +96,7 @@ begin
 	uut_predecode_logic: predecode_logic
 		port map (
 			i_clk => t_clk_2,
-			i_assert_interrupt_control => '0',  -- No interrupt control in this testbench
+			i_assert_interrupt_control => '0',	-- No interrupt control in this testbench
 			i_fetch => t_fetch,
 			i_pr_instruction => t_pr_instruction,
 			o_pl_instruction => t_pl_instruction,
@@ -107,16 +107,16 @@ begin
 	-- Instantiate instruction_register module
 	uut_instruction_register: instruction_register
 	port map (
-		i_clk => t_clk_1,  -- Use the same clock as predecode_register
+		i_clk => t_clk_1,	-- Use the same clock as predecode_register
 		i_reset => '0',	 -- No reset in this testbench
 		i_pl_instruction => t_pl_instruction,
-		o_ir_instruction => open  -- Connect output to a signal or observe it in simulation
+		o_ir_instruction => open	-- Connect output to a signal or observe it in simulation
 	);
 
 	-- Instantiate decode_rom module
 	uut_decode_rom: decode_rom
 	port map (
-		ir => t_pl_instruction,  -- Connect to the output of predecode_logic module
+		ir => t_pl_instruction,	-- Connect to the output of predecode_logic module
 		t_n => t_timing_n,	 -- Connect to the output of timing_generator module
 		pla => t_pla_output	 -- Connect to a signal to observe the output or connect it to other modules as needed
 	);
@@ -138,14 +138,14 @@ begin
 	-- Clock processes
 	clk_process: process
 	begin
-        while now < 1000 ns loop  -- Simulate for 1000 ns
-            t_clk_1 <= '1';  -- Set t_clk_1 to high state
-            t_clk_2 <= '0';  -- Set t_clk_2 to low state
-            wait for CLK_PERIOD / 2;
-            t_clk_1 <= '0';  -- Set t_clk_1 to low state
-            t_clk_2 <= '1';  -- Set t_clk_2 to high state
-            wait for CLK_PERIOD / 2;
-        end loop;
+		while now < 1000 ns loop	-- Simulate for 1000 ns
+			t_clk_1 <= '1';	-- Set t_clk_1 to high state
+			t_clk_2 <= '0';	-- Set t_clk_2 to low state
+			wait for CLK_PERIOD / 2;
+			t_clk_1 <= '0';	-- Set t_clk_1 to low state
+			t_clk_2 <= '1';	-- Set t_clk_2 to high state
+			wait for CLK_PERIOD / 2;
+		end loop;
 		wait;
 	end process clk_process;
 
