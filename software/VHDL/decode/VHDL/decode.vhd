927bf9f890253b4aab5fb295f88f24c94ab6f1e1
NightFore <Et3rnNighTaLL@Hotmail.com>
2024-02-27 00:31:46 +0100

    #12 Implemented decode_rom & Updated decode

--- software/VHDL/decode/VHDL/decode.vhd
+++ software/VHDL/decode/VHDL/decode.vhd
@@ -14,7 +14,11 @@ entity decode is
 		o_pl_instruction		: out std_logic_vector(7 downto 0);
 		o_pl_implied			: out std_logic;
 		o_pl_tzpre				: out std_logic;
-		o_ir_instruction		: out std_logic_vector(7 downto 0)
+		o_ir_instruction		: out std_logic_vector(7 downto 0);
+		o_tgl_timing_n  		: out std_logic_vector(5 downto 0);
+		o_tgl_fetch				: out std_logic;
+		o_tgl_sync				: out std_logic;
+		o_dr_pla				: out std_logic_vector(129 downto 0)
 	);
 end decode;
 
@@ -66,6 +70,15 @@ architecture Behavioral of decode is
 		);
 	end component;
 
+	-- Component declaration for decode_rom
+	component decode_rom is
+		port (
+			i_ir_instruction	: in std_logic_vector(7 downto 0);
+			i_tgl_timing_n		: in std_logic_vector(5 downto 0);
+			o_dr_pla			: out std_logic_vector(129 downto 0)
+		);
+	end component;
+
 	-- Signal for output from predecode_register
 	signal s_pr_instruction		: std_logic_vector(7 downto 0);
 
@@ -81,6 +94,9 @@ architecture Behavioral of decode is
 	signal s_tgl_timing_n		: std_logic_vector(5 downto 0);
 	signal s_tgl_fetch			: std_logic;
 	signal s_tgl_sync			: std_logic;
+
+	-- Signals for decode_rom outputs
+	signal s_dr_pla				: std_logic_vector(129 downto 0);
 
 begin
 	-- Instantiate predecode_register module
@@ -125,6 +141,14 @@ begin
 		o_tgl_sync			=> s_tgl_sync
 	);
 
+	-- Instantiate decode_rom module
+	UUT_decode_rom: decode_rom
+	port map (
+		i_ir_instruction	=> s_ir_instruction,
+		i_tgl_timing_n		=> s_tgl_timing_n,
+		o_dr_pla			=> s_dr_pla
+	);
+
 	-- Assign output signal from predecode_register to the top-level output port
 	o_pr_instruction		<= s_pr_instruction;
 
@@ -137,8 +161,11 @@ begin
 	o_ir_instruction		<= s_ir_instruction;
 
 	-- Assign output signal from timing_generator_logic to the top-level output port
-    o_pl_instruction		<= s_pl_instruction;
-    o_pl_implied			<= s_pl_implied;
-    o_pl_tzpre				<= s_pl_tzpre;
+    o_tgl_timing_n			<= s_tgl_timing_n;
+    o_tgl_fetch				<= s_tgl_fetch;
+    o_tgl_sync				<= s_tgl_sync;
+
+	-- Assign output signal from decode_rom to the top-level output port
+    o_dr_pla				<= s_dr_pla;
 
 end Behavioral;
