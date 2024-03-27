539bcc898bea2608a9bfabf284433323ac18ca8f
Robaur <auriac.robin.pro@gmail.com>
2024-03-27 22:23:34 +0100

    finaly the true ram is come

--- software/VHDL/CODEC/Codec.vhd
+++ software/VHDL/CODEC/Codec.vhd
@@ -14,5 +14,3 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
-entity Codec is 
--- software/VHDL/RAM/RAM.vhd
+++ software/VHDL/RAM/RAM.vhd
@@ -7,26 +7,28 @@ port(
     clk: in std_logic;
     w_en: in std_logic; 
     data : inout std_logic_vector(7 downto 0);
-    adress: in std_logic_vector(15 downto 0):
+    adress: in std_logic_vector(12 downto 0)
 );
 end RAM;
-    subtype DAT is unsigned (7 downto 0);
-    type MEMORY is array (2**15 downto 0) of DAT
-    signal reg : MEMORY;
-    
+
+architecture RAM_rtf of RAM is 
+    subtype DAT is std_logic_vector (7 downto 0);
+    type MEMORY is array (0 to 2047) of DAT;
+    signal reg : MEMORY:=(others => "00000000");
+
     begin 
 
     process(clk)
     begin
         if (rising_edge(clk))then 
             if (w_en='1') then
-                reg(to_integer(adress))<=data;
+                reg(to_integer(unsigned(adress(11 downto 0))))<=data;
             else 
-                data<=reg(to_integer(adress));
+                data<=reg(to_integer(unsigned(adress(11 downto 0))));
             end if;
         end if;
     end process; 
 
-architecture RAM_rtf of RAM is 
+
 
 end RAM_rtf; 
